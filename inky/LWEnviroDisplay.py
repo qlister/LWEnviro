4a89ca2624345b9f12bf89b92a57ebf7a4f4bf87
Quentin Lister <quentinlister@gmail.com>
2022-10-09 18:32:52 +0100

    Work on putting data into a database Also added Pimoroni urban

--- inky/LWEnviroDisplay.py
+++ inky/LWEnviroDisplay.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 import paho.mqtt.client as mqtt   # Import MQTT Library
 import time
 import json
@@ -9,7 +9,7 @@ import LWInky
 msgQ = Queue()
 
 def on_message(cl, userdata, message):
-    #    print("message received " ,str(message.payload.decode("utf-8")))
+#    print("message received " ,str(message.payload.decode("utf-8")))
     msgQ.put(json.loads(message.payload.decode("utf-8")))
 
     #import matplotlib.pyplot as plt
@@ -17,8 +17,8 @@ def on_message(cl, userdata, message):
     #plt.savefig('foo.png')
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected flags ",str(flags),"result code ",str(rc))
-
+#    print("Connected flags ",str(flags),"result code ",str(rc))
+    pass
 
 myInky = LWInky.LWdisplay()
 myDB = SQLiteDB.db()
@@ -31,21 +31,38 @@ client = mqtt.Client("P1", True) #create new instance
 client.username_pw_set("mos-serv", "L1qu0r1ce")
 client.connect(broker_address) #connect to broker
 client.subscribe("enviro/kitchen")
+client.subscribe("enviro/outdoor")
 client.on_message=on_message        #attach function to callback
 client.on_connect = on_connect
 
 client.loop_start()    #start the loop
 
+then = datetime.now()
+then = datetime.now().minute
+print ( 'then = ' + str(then) )
 
 while True:
+    
+    now = datetime.now().minute
+    print ( 'now = ' + str(now) )
+    if now != then:
+        then = now
+        myInky.draw( )
+        
     while not msgQ.empty():   # This will sequentially read out queued messages
         data = msgQ.get()
-#        print( str(readings) )      
-        myDB.add_kitchen_data( data )
         
-        myInky.draw( data )
+        m = data['model']
+#        print( 'Model = ' + m )
+        
+        if m == 'urban':
+            myDB.add_urban_data( data )
+        elif m == 'indoor':
+            myDB.add_kitchen_data( data )
+        else:
+            print("Remote MQTT device not recognised")
 
-    time.sleep(0.5)
+    time.sleep( 0.5 )    # check the queue twice a second (approx)
 
 
 
--- inky/SQLiteDB.py
+++ inky/SQLiteDB.py
@@ -1,5 +1,5 @@
 import sqlite3
-from datetime import datetime
+#from datetime import datetime
 
 
 class db:
@@ -10,14 +10,33 @@ class db:
 
     def create_blank(self):
         self.cur.execute("DROP TABLE IF EXISTS kitchen")
-        self.cur.execute("CREATE TABLE kitchen(id INTEGER PRIMARY KEY AUTOINCREMENT, pressure REAL, temprature REAL, humidity REAL, luminance INTEGER, colour_temp INTEGER, date DATETIME)")
+        self.cur.execute("CREATE TABLE kitchen(id INTEGER PRIMARY KEY AUTOINCREMENT, pressure REAL, temprature REAL, humidity REAL, luminance INTEGER, colour_temp INTEGER, uid VARCHAR(20), date DATETIME)")
+        self.cur.execute("DROP TABLE IF EXISTS urban_outdoor")
+        self.cur.execute("CREATE TABLE urban_outdoor(id INTEGER PRIMARY KEY AUTOINCREMENT, pressure REAL, temprature REAL, humidity REAL, pm1 INTEGER, pm2_5 INTEGER, pm10 INTEGER, noise REAL, voltage REAL, uid VARCHAR(20), date DATETIME)")
     
     def add_kitchen_data( self, data ):
-        readings = data["readings"]
-        sql = 'INSERT INTO kitchen (pressure, temprature, humidity, luminance, colour_temp, date) VALUES '
-        sql += ( '( %f, %f, %f, %d, %d, "%s" )' % ( readings["pressure"], readings["temperature"], readings["humidity"], readings["luminance"], readings["color_temperature"], str(data["timestamp"]) ) )
-        print( sql )
-        self.cur.execute( sql )
+        if data['model']  == 'indoor':
+            readings = data["readings"]
+            sql = 'INSERT INTO kitchen (pressure, temprature, humidity, luminance, colour_temp, uid, date) VALUES '
+            sql += ( '( %f, %f, %f, %d, %d, "%s", "%s" )' % ( readings["pressure"], readings["temperature"], readings["humidity"], readings["luminance"], readings["color_temperature"], data["uid"], str(data["timestamp"]) ) )
+#            print( sql )
+            self.cur.execute( sql )
+            self.con.commit()
+        else:
+            print( "Unexpected model in add_kitchen_data" )
+            print ( data )
+        
+    def add_urban_data( self, data ):
+        if data['model']  == 'urban':
+            readings = data["readings"]
+            sql = 'INSERT INTO urban_outdoor (pressure, temprature, humidity, pm1, pm2_5, pm10, noise, voltage, uid, date) VALUES '
+            sql += ( '( %f, %f, %f, %d,%d, %d, %f, %f, "%s", "%s" )' % ( readings["pressure"], readings["temperature"], readings["humidity"], readings["pm1"], readings["pm2_5"], readings["pm10"], readings["noise"], readings["voltage"], data["uid"], str(data["timestamp"]) ) )
+#            print( sql )
+            self.cur.execute( sql )
+            self.con.commit()
+        else:
+            print( "Unexpected model in add_urban_data" )
+            print ( data )
     
 
 
